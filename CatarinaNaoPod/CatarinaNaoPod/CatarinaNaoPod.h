ed596da3bd59ddefc63db3d70b39f692c30a5fdd
ktchaos <marialacerda@cc.ci.ufpb.br>
2021-12-13 13:15:21 -0300

    initial commit

---
+++ CatarinaNaoPod/CatarinaNaoPod/CatarinaNaoPod.h
@@ -0,0 +1,18 @@
+//
+//  CatarinaNaoPod.h
+//  CatarinaNaoPod
+//
+//  Created by Catarina Serrano on 13/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CatarinaNaoPod.
+FOUNDATION_EXPORT double CatarinaNaoPodVersionNumber;
+
+//! Project version string for CatarinaNaoPod.
+FOUNDATION_EXPORT const unsigned char CatarinaNaoPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatarinaNaoPod/PublicHeader.h>
+
+
---
+++ CatarinaNaoPod/CatarinaNaoPod/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  CatarinaNaoPod
+//
+//  Created by Catarina Serrano on 13/12/21.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+
+    public static func doSomething() -> String {
+        return "Did something fool"
+    }
+}
